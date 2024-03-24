62664454951e6aadc925f19095ecb2022da96f0b
Jonas Frey <jonfrey@ethz.ch>
2024-03-24 19:55:24 +0100

    fixed pytest

--- tests/test_svd_timing.py
+++ tests/test_svd_timing.py
@@ -1,24 +1,29 @@
 from pytictac import Timer
 import torch
 
-BS = 1024
-repeats = 100
 
-for i in range(repeats):
-    m = torch.rand((BS, 3, 3), device="cuda")
-    u, s, v = torch.svd(m)
-
-with Timer("SVD"):
+def test_svd():
+    BS = 1024
+    repeats = 100
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     for i in range(repeats):
-        m = torch.rand((BS, 3, 3), device="cuda")
+        m = torch.rand((BS, 3, 3), device=device)
         u, s, v = torch.svd(m)
 
-m = torch.rand((BS, 3, 3), device="cuda")
-with Timer("SVD"):
-    for i in range(repeats):
+    with Timer("SVD"):
+        for i in range(repeats):
+            m = torch.rand((BS, 3, 3), device=device)
+            u, s, v = torch.svd(m)
+
+    m = torch.rand((BS, 3, 3), device=device)
+    with Timer("SVD"):
+        for i in range(repeats):
+            u, s, v = torch.svd(m)
+
+    m = torch.rand((BS, 3, 3), device=device)
+    with Timer("SVD single"):
         u, s, v = torch.svd(m)
 
 
-m = torch.rand((BS, 3, 3), device="cuda")
-with Timer("SVD single"):
-    u, s, v = torch.svd(m)
+if __name__ == "__main__":
+    test_svd()
