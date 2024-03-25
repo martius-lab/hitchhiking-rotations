3659848f1f0be0e96e93c78dfdb4f42132add203
Jonas Frey <jonfrey@ethz.ch>
2024-03-25 08:41:05 +0100

    updated for pytest

--- tests/test_svd_timing.py
+++ tests/test_svd_timing.py
@@ -1,27 +1,33 @@
-from pytictac import Timer
+from pytictac import Timer, CpuTimer
 import torch
 
 
 def test_svd():
-    BS = 1024
+    BS = 128
     repeats = 100
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available():
+        tim = Timer
+    else:
+        tim = CpuTimer
+
     for i in range(repeats):
         m = torch.rand((BS, 3, 3), device=device)
         u, s, v = torch.svd(m)
 
-    with Timer("SVD"):
+    with tim("SVD"):
         for i in range(repeats):
             m = torch.rand((BS, 3, 3), device=device)
             u, s, v = torch.svd(m)
 
     m = torch.rand((BS, 3, 3), device=device)
-    with Timer("SVD"):
+    with tim("SVD"):
         for i in range(repeats):
             u, s, v = torch.svd(m)
 
     m = torch.rand((BS, 3, 3), device=device)
-    with Timer("SVD single"):
+    with tim("SVD single"):
         u, s, v = torch.svd(m)
 
 
